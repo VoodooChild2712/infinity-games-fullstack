dc277358ce1ce4bff001e0ae83fee9d74718cb22
Gianfranco Marzulli <marzulligianfranco@gmail.com>
2023-07-28 16:18:23 +0200

    added structure and styles to the layout

--- src/pages/layout/footer/index.jsx
+++ src/pages/layout/footer/index.jsx
@@ -1,10 +1,16 @@
-import React from 'react';
-import styles from '../index.module.css'
+import React from "react";
+import styles from "../index.module.css";
+import { Link } from "react-router-dom";
+import ContactUs from "../../contact-us/index";
 
 const Footer = () => {
   return (
-    <div className={styles.footer}>Footer</div>
-  )
-}
+    <div className={styles.footer}>
+      <h1 className={styles.footerTitle}>INFINITY GAMES</h1>
+      <Link to={ContactUs} className={styles.contactButton}>Contact Us</Link>
+      <div className={styles.mediaRefs}></div>
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
--- src/pages/layout/navbar/index.jsx
+++ src/pages/layout/navbar/index.jsx
@@ -1,10 +1,22 @@
-import React from 'react'
-import styles from '../index.module.css'
+import React from "react";
+import styles from "../index.module.css";
+import { Link } from "react-router-dom";
+import Register from "../../register/index";
+import Login from "../../login/index";
 
 const Navbar = () => {
   return (
-    <div className={styles.navbar}>Navbar</div>
-  )
-}
+    <div className={styles.navbar}>
+      <div className={styles.navModal}></div>
+      <div className={styles.navTitleContainer}>
+        <h1 className={styles.navTitle}>INFINITY GAMES</h1>
+      </div>
+      <div className={styles.navButtons}>
+        <Link to={Login} className={styles.loginButton}>Login</Link>
+        <Link to={Register} className={styles.registerButton}>Register</Link>
+      </div>
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;
