bfffb8faf011f0ed654e96781ec4add7b13d28e2
Gianfranco Marzulli <marzulligianfranco@gmail.com>
2023-07-29 17:31:28 +0200

    added the carousel

--- src/pages/components/carousel/index.jsx
+++ src/pages/components/carousel/index.jsx
@@ -1,18 +1,20 @@
 import React from "react";
-import styles from "./index.module.css";
 import Slider from "react-slick";
+import image from "../../../logo.svg";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import styles from "./index.module.css";
 
 const Carousel = () => {
   const carouselSettings = {
-    dots: true, // Show navigation dots
-    infinite: true, // Enable infinite loop
-    speed: 500, // Transition speed in milliseconds
-    slidesToShow: 3, // Number of slides to show at once
-    slidesToScroll: 1, // Number of slides to scroll on each navigation
-    autoplay: true, // Auto-play the carousel
-    autoplaySpeed: 3000, // Time between slides during auto-play
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 300,
     responsive: [
-      // Responsive settings for different screen sizes
       {
         breakpoint: 1024,
         settings: {
@@ -29,17 +31,16 @@ const Carousel = () => {
   };
 
   return (
-    <div>
-      <h2>My Carousel</h2>
+    <div className={styles["carousel-container"]}>
       <Slider {...carouselSettings}>
         <div>
-          <img src="carousel-image-1.jpg" alt="Slide 1" />
+          <img src={image} alt="Slide 1" />
         </div>
         <div>
-          <img src="carousel-image-2.jpg" alt="Slide 2" />
+          <img src={image} alt="Slide 2" />
         </div>
         <div>
-          <img src="carousel-image-3.jpg" alt="Slide 3" />
+          <img src={image} alt="Slide 3" />
         </div>
       </Slider>
     </div>
